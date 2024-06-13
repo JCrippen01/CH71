513d6382c3f3ba99a6d911f1df22008dd42b017e
Justin Crippen <justin.crippen@gmail.com>
2024-06-13 02:16:51 -0500

    Thinking Chapter

---
+++ chapter_1/thinking/switch.py
@@ -0,0 +1,10 @@
+#The issue: I need my apples to output 11, and your apples to output 5.
+myApples = 5
+yourApples = 11
+
+#to switch variables I am going to use pythons multiple variable solution in (x,y) format and set that equal to its opposit (y,x)
+(myApples, yourApples) = (yourApples, myApples)
+# I will then need to display the output.
+
+print(myApples)
+print(yourApples)
